06e399c7403fd7525c88f78857643cde4b03b9c1
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2021-11-14 19:11:13 +0530

    last rundown

--- js/engine.js
+++ js/engine.js
@@ -1,14 +1,15 @@
 let fed = document.getElementById("feed").value;
 let box = document.getElementById("box");
-// const add = () => {
-//   let para = document.createElement("p");
-//   //   let para = document.getElementsByName("p");
-//   let text = document.createTextNode(fed);
-//   para.appendChild(text);
-//   //   box.appendChild(para);
-//   console.log(text);
-// };
-
 const add = () => {
-  console.log(fed);
+  let para = document.createElement("p");
+  //   let para = document.getElementsByName("p");
+  let text = document.createTextNode(fed);
+  // console.log(text)
+  para.appendChild(text);
+  console.log(para)
+    box.appendChild(para);
 };
+
+// const add = () => {
+//   console.log(fed);
+// };
