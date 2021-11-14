aa16672e6223c87f4df029fccc94dc6555b9490d
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2021-11-14 21:09:30 +0530

    last rundown and adding stuff is done

--- js/engine.js
+++ js/engine.js
@@ -1,15 +1,30 @@
-let fed = document.getElementById("feed").value;
+// let fed = document.getElementById("feed").value;
+// it doesn't work coz we grabbing the text after click before that input is an empty string
 let box = document.getElementById("box");
 const add = () => {
+  let fed = document.getElementById("feed").value;
+  // it works now coz we grabbing the text after clickingso now input has a string
   let para = document.createElement("p");
   //   let para = document.getElementsByName("p");
   let text = document.createTextNode(fed);
   // console.log(text)
-  para.appendChild(text);
-  console.log(para)
+  if (fed.length > 0) {
+    para.appendChild(text);
+    console.log(para);
     box.appendChild(para);
+  } else {
+    alert("please try to feed something before entering");
+  }
 };
 
 // const add = () => {
 //   console.log(fed);
 // };
+
+// if (fed.length > 0) {
+//   para.appendChild(text);
+//   console.log(para);
+//   box.appendChild(para);
+// } else {
+//   alert("please try to feed something before entering");
+// }
