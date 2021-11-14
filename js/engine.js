029d657063829dd854af763d3d9c281682c4f1d0
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2021-11-14 15:07:21 +0530

    working

--- js/engine.js
+++ js/engine.js
@@ -1,35 +1,14 @@
-let count = document.getElementById("count");
-let sum = document.getElementById("sum");
-let log = document.getElementById("log");
-let counter = 0;
-let total = 0;
-const add = () => {
-  counter = counter + 1;
-  total = counter * 5;
-  count.innerText = counter;
-  sum.innerText = total;
-  let tag = document.createElement("p");
-  tag.id = "logger";
-  let text = document.createTextNode("Passenger came in");
-  tag.appendChild(text);
-  log.appendChild(tag);
-};
-const remove = () => {
-  let tag = document.createElement("p");
-  tag.id = "logger";
-  let text = document.createTextNode("Passenger left");
-  if (counter > 0) {
-    counter = counter - 1;
-    console.log(counter);
-    total = total - 5;
-    tag.appendChild(text);
-  } else {
-    let text = document.createTextNode("Bus Empty");
-    tag.appendChild(text);
-  }
-  count.innerText = counter;
-  sum.innerText = total;
-  log.appendChild(tag);
-};
+let fed = document.getElementById("feed").value;
+let box = document.getElementById("box");
+// const add = () => {
+//   let para = document.createElement("p");
+//   //   let para = document.getElementsByName("p");
+//   let text = document.createTextNode(fed);
+//   para.appendChild(text);
+//   //   box.appendChild(para);
+//   console.log(text);
+// };
 
-console.log(count);
+const add = () => {
+  console.log(fed);
+};
