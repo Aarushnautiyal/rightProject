8bd7738d4e7b54de4a9a43000d4ef594f3668846
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2021-11-19 13:04:26 +0530

    todo

--- js/engine.js
+++ js/engine.js
@@ -1,22 +1,33 @@
 // let fed = document.getElementById("feed").value;
 // it doesn't work coz we grabbing the text after click before that input is an empty string
 let box = document.getElementById("box");
-const add = () => {
+const list = ["my list", "text"];
+const add = (render) => {
   let fed = document.getElementById("feed").value;
   // it works now coz we grabbing the text after clickingso now input has a string
-  let para = document.createElement("p");
+  // let para = document.createElement("p");
   //   let para = document.getElementsByName("p");
   let text = document.createTextNode(fed);
   // console.log(text)
   if (fed.length > 0) {
-    para.appendChild(text);
-    console.log(para);
-    box.appendChild(para);
+    // para.appendChild(text);
+    // console.log(para);
+    // box.appendChild(para);
+    box.innerHTML += `<p>${fed}</p>`;
+    list.push(fed);
+    render();
+    fed = "";
   } else {
     alert("please try to feed something before entering");
   }
 };
-
+// const render = () => {
+//   for (let i = 0; i < list.length; i++) {
+//     let text = "";
+//     text += `<p>${list[i]}</p>`;
+//   }
+//   box.innerHTML += text;
+// };
 // const add = () => {
 //   console.log(fed);
 // };
